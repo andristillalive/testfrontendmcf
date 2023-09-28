2f84eb0dc92ae97b52bf2366020117eda7f9ce2d
andri <vengefoldjrstar@gmail.com>
2023-09-29 03:04:19 +0700

    first-commit

---
+++ FrontEndMCF/Controllers/HomeController.cs
@@ -0,0 +1,69 @@
+﻿using FrontEndMCF.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using System.Text;
+using System.Text.Json;
+
+namespace FrontEndMCF.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost("InsertHome")]
+        public JsonResult InsertHome(RequestInsertData rid)
+        {
+            dynamic result = InsertDataFromBackend(rid);
+            return Json(result);
+        }
+
+        private object InsertDataFromBackend(RequestInsertData rid)
+        {
+            using (HttpClient httpClient = new HttpClient())
+            {
+                string url = "https://localhost:7253/api/Home/InsertData";
+                using StringContent jsonContent = new(JsonSerializer.Serialize(rid), Encoding.UTF8, "application/json");
+                return Task.Run(() => PostAsynchronous(httpClient, url, jsonContent));
+            }
+        }
+
+        private async Task<object> PostAsynchronous(HttpClient httpClient, string url, StringContent sc)
+        {
+            using HttpResponseMessage response = await httpClient.PostAsync(url, sc);
+            string result = await response.Content.ReadAsStringAsync();
+            return new { result };
+        }
+
+        public class RequestInsertData
+        {
+            public string AgreementNumber { get; set; } = null!;
+
+            public string? BpkbNo { get; set; }
+
+            public string? BranchId { get; set; }
+
+            public DateTime? BpkbDate { get; set; }
+
+            public string? FakturNo { get; set; }
+
+            public DateTime? FakturDate { get; set; }
+
+            public string? LocationId { get; set; }
+
+            public string? PoliceNo { get; set; }
+
+            public DateTime? BpkbDateIn { get; set; }
+
+            public string? CreatedBy { get; set; }
+
+            public DateTime? CreatedOn { get; set; }
+
+            public string? LastUpdateBy { get; set; }
+
+            public DateTime? LastUpdateOn { get; set; }
+        }
+    }
+}
---
+++ FrontEndMCF/Controllers/LoginController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Net.Http.Json;
+
+namespace FrontEndMCF.Controllers
+{
+    public class LoginController : Controller
+    {
+        [HttpGet("Login")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet("LoginGET")]
+        public JsonResult Login(string username, string password) 
+        {
+            dynamic result = GetLoginFromBackend(username,password);
+            return Json(result);
+        }
+
+        private object GetLoginFromBackend(string username, string password)
+        {
+            using (HttpClient httpClient = new HttpClient())
+            {
+                string url = "https://localhost:7253/api/Login/Login?username=" + username + "&password=" + password;
+                return Task.Run(() => GetAsynchronous(httpClient,url));
+            }
+        }
+
+        private async Task<object> GetAsynchronous(HttpClient httpClient, string url)
+        {
+            using HttpResponseMessage response = await httpClient.GetAsync(url);
+            string result = await response.Content.ReadAsStringAsync();
+            return new { result };
+        }
+    }
+}
